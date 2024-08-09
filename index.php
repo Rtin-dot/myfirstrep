197701e1376b83c0ea4611b27fc231bc86d5536d
Rtin-dot <artimorxnorant1@gmail.com>
2024-08-09 23:25:06 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Nigga ordering Form </title>
+</head>
+<body>
+    
+    <div class="container">
+     <h1>ordring form</h1>
+        <form action="index.php" method="post">
+            <label>How many niggas do you want?</label>
+            <input type="number" name="nigga" required>
+            <br>
+            <input type="submit" value="Buy">
+        </form>
+        
+    </div>
+</body>
+</html>
