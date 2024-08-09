f662cd63fb435d25fcc290ceb310da0ec0dbd775
Rtin-dot <artimorxnorant1@gmail.com>
2024-08-09 22:35:59 +0330

    index.php

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
-    <title>Nigga ordering Form </title>
-</head>
-<body>
-    
-    <div class="container">
-     <h1>ordring form</h1>
-        <form action="index.php" method="post">
-            <label>How many niggas do you want?</label>
-            <input type="number" name="nigga" required>
-            <br>
-            <input type="submit" value="Buy">
-        </form>
-        
-        <div class="result">
             <?php 
                 
                 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -40,7 +20,3 @@
                     }
                 }
             ?>
-        </div>
-    </div>
-</body>
-</html>
